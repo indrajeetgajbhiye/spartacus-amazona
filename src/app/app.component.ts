1f3e4d83de277c7e344c21159d54b44bcb05aaa3
Indrajeet Gajbhiye <indrajeetgajbhiye@gmail.com>
2022-05-04 19:42:38 +0530

    session-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
+import { OutletPosition, OutletService } from '@spartacus/storefront';
+import { factory } from 'typescript';
+import { TestSpartacusComponent } from './features/outlets/test-spartacus/test-spartacus.component';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +12,21 @@ export class AppComponent {
   title = 'spartacus-amazona';
   value: number = 0;
   show: boolean = true;
+
+  constructor(
+    private componenntFactoryResolver: ComponentFactoryResolver,
+    private outletService: OutletService
+  ) {
+    const factory = this.componenntFactoryResolver.resolveComponentFactory(
+      TestSpartacusComponent
+    );
+    this.outletService.add('SiteLogo', factory, OutletPosition.AFTER);
+  }
   changeValue() {
     this.value = this.value + 1;
   }
   toggle() {
+    console.log('toggle');
     this.show = !this.show;
   }
 }
